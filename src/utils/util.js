48b27974778fb516b015c6abd58c810c431eb71c
guang.shi <shiguang0116@163.com>
2019-05-01 00:53:32 +0800

    部分文件格式优化

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,4 +1,6 @@
 /**
- * @download    npm i sg-utils -S
- * @github      https://github.com/shiguang0116/sg-utils
+ * @description: js通用工具类
+ * 
+ * @download:    npm i sg-utils -S
+ * @github:      https://github.com/shiguang0116/sg-utils
  */
